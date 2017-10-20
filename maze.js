afefc142606c64ece5490c29c085985828cffe12
patellaportuondo <32628686+patellaportuondo@users.noreply.github.com>
2017-10-20 18:13:29 -0500

    Ex3

--- maze.js
+++ maze.js
@@ -1,6 +1,7 @@
 function eventHandler() {
 	var boundary1 = document.getElementById("boundary1");
 	var otherBoundaries = document.querySelectorAll(".boundary");
+	var end = document.getElementById("end");
 
 	boundary1.onmouseover=function(){
 		boundary1.classList.add("youlose");
@@ -14,6 +15,13 @@ function eventHandler() {
 		}
 	}
 
+	end.onmouseover=function(){
+		if (boundary1.getAttribute("class")=="boundary youlose") {
+			alert("YOU LOSE!");
+		} else {
+			alert("YOU WIN!");
+		}
+	}
 }
   
 window.onload=function(){
