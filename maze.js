e8b03082e6f067d82489631b450d4d7b0fb07d19
patellaportuondo <32628686+patellaportuondo@users.noreply.github.com>
2017-10-20 18:01:41 -0500

    Ex2

--- maze.js
+++ maze.js
@@ -1,9 +1,19 @@
 function eventHandler() {
 	var boundary1 = document.getElementById("boundary1");
+	var otherBoundaries = document.querySelectorAll(".boundary");
 
 	boundary1.onmouseover=function(){
 		boundary1.classList.add("youlose");
 	}
+
+	for (var i = 0; i < otherBoundaries.length-1; i++) {
+		otherBoundaries[i].onmouseover = function(){
+			for (var i = 0; i< otherBoundaries.length-1; i++) {
+			otherBoundaries[i].classList.add("youlose");
+			}
+		}
+	}
+
 }
   
 window.onload=function(){
